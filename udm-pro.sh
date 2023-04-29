5274d9294d31aeb865f202ea81640ad019770a7b
Scott Helme <scott.helme@gmail.com>
2023-04-29 16:53:24 +0100

    Use correct paths for Unifi OS >= 2.0

--- udm-pro.sh
+++ udm-pro.sh
@@ -1,3 +1,3 @@
-scp chain.crt root@192.168.1.1:/mnt/.rwfs/data/data/unifi-core/config/unifi-core.crt
-scp private.key root@192.168.1.1:/mnt/.rwfs/data/data/unifi-core/config/unifi-core.key
+scp chain.crt root@192.168.1.1:/data/unifi-core/config/unifi-core.crt
+scp private.key root@192.168.1.1:/data/unifi-core/config/unifi-core.key
 ssh root@192.168.1.1 'systemctl restart unifi-core'
